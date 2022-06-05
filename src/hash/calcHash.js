5a363977c421f6a5700e5562a5b181d4a4eb50d2
Mironov Vladimir <zidiks228@gmail.com>
2022-06-05 12:54:48 +0300

    upd: finish hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,29 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import crypto from 'crypto';
+import fs from "fs/promises";
+import { constants } from "fs";
+
+const filePath = './files/fileToCalculateHashFor.txt';
+
+export const calculateHash = () => {
+    return checkPathExists(filePath).then((exists) => {
+        if (exists) {
+            return fs.readFile(filePath, { encoding: 'utf8' }).then((content) => {
+                return hash(content);
+            });
+        } else {
+            throw 'FS operation failed';
+        }
+    });
 };
+
+function hash(data) {
+    return crypto.createHash('sha256').update(data).digest('hex');
+}
+
+function checkPathExists(file) {
+    return fs.access(file, constants.F_OK)
+        .then(() => true)
+        .catch(() => false)
+}
+
+calculateHash().then((hash) => console.log(hash));
