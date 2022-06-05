755ae1d83512603d5a7d0207f931c16c624504f5
Mironov Vladimir <zidiks228@gmail.com>
2022-06-05 16:24:59 +0300

    upd: finish zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createGzip } from "zlib";
+
+const filePath = "./files/fileToCompress.txt";
+const gzipFilePath = "./files/archive.gz";
+
 export const compress = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(gzipFilePath);
+    const readStream = createReadStream(filePath);
+    const gzip = createGzip();
+
+    return pipeline(readStream, gzip, writeStream);
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,16 @@
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createGunzip } from "zlib";
+
+const gzipFilePath = "./files/archive.gz";
+const filePath = "./files/fileToCompress.txt";
+
 export const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(gzipFilePath);
+    const writeStream = createWriteStream(filePath);
+    const unGzip = createGunzip();
+
+    return pipeline(readStream, unGzip, writeStream);
 };
+
+decompress();
