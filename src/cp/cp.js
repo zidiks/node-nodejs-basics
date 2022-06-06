211a5ea2f5b2c213cc1010c40b6dfac28a09fceb
Zidiks <zidiks228@gmail.com>
2022-06-06 17:28:03 +0300

    upd: finish cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,17 @@
+import { fork } from "child_process";
+
+const scriptPath = './files/script.js';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(scriptPath, args, { silent: true });
+
+    childProcess.on("spawn", () => console.log('Spawn child process'))
+    childProcess.on("exit", (code) => console.log('Exit with: ' + code));
+    childProcess.stdout.pipe(process.stdout);
+    childProcess.stdout.on("data", (chunk) => console.log(`Message from child process: ${chunk}`));
+    process.stdin.pipe(childProcess.stdin);
 };
+
+await spawnChildProcess();
+
+
